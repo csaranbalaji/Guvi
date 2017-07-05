2c9668773121af8490552cbc53ee0e50e542bde8
Saran Balaji <csaranbalaji@gmail.com>
2017-07-05 19:49:41 +0530

    edit count.py

--- count.py
+++ count.py
@@ -1,1 +1,6 @@
-http://www.geeksforgeeks.org/find-duplicates-in-on-time-and-constant-extra-space/
+r=[]
+a=map(int,raw_input().strip().split())
+for i in a:
+	if a.count(i)>1:
+		r.append(i)
+print len(set(r))
