4d9b55035cc85b85d495e5d250861de0f35537d9
Saran Balaji <csaranbalaji@gmail.com>
2017-07-11 18:30:11 +0530

    Update alternate reverse.py

--- alternate reverse.py
+++ alternate reverse.py
@@ -2,4 +2,4 @@ sentence=input().strip().split(' ')		#Get the input in an array seperated by spa
 for i in range(len(sentence)):
 	if i%2:
 		sentence[i]=sentence[i][::-1] 		#To reverse the alternate string
-print(*sentence)
+print(*sentence)				#Print the array seperated by spaces
