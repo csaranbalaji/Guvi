5898a213b30b0a1e42cf965557e295cbbbdf1844
Saran Balaji <csaranbalaji@gmail.com>
2017-07-22 06:51:25 +0530

    added unit digit.py

--- emperor.py
+++ emperor.py
@@ -1,2 +1,15 @@
 n=input()
-a=map(int,raw_input().strip().split())
+a=[1]
+l=map(int,raw_input().strip().split())
+a+=l
+a.append(1)
+c=[]+l
+i1=a.index(max(l))
+del l[i1]
+i2=a.index(max(l))
+del l[i2]
+if i1>i2:
+	i1,i2=i2,i1
+while(i1+1!=i2):
+	o.append(min(a[i1:i2]]))
+	
---
+++ unit digit.py
@@ -0,0 +1,7 @@
+s,e,d=input(),input(),input()
+for i in range(s,s+10):
+    if i%10==d:
+        s=i
+        break
+for i in range(s,e+1,10):
+    print i
