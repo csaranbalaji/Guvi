d0622b4e81eec536009ac1c70c04308bed0c1a1b
Saran Balaji <csaranbalaji@gmail.com>
2017-07-27 06:43:24 +0530

    edit BST.py

--- bst.py
+++ bst.py
@@ -1,4 +1,8 @@
-class bst:
-	def __init__():
-		
+class Node:
+    rChild,lChild,data = None,None,None
+
+    def __init__(self,key):
+        self.rChild = None
+        self.lChild = None
+        self.data = key
 	
