ee479211ff15f8fd61306021cb46af7cab10a8c4
Saran Balaji <csaranbalaji@gmail.com>
2017-08-04 07:02:19 +0530

    edit country leader.py

--- country leader.py
+++ country leader.py
@@ -5,7 +5,7 @@ for tc in range(input()):
 	n.sort()
 	p,m='',0
 	for i in n:
-		if len(set(i.strip()))>m:
-			m=len(set(i.strip()))
+		if len(set(i.replace(' ','')))>m:
+			m=len(set(i.replace(' ','')))
 			p=i
 	print "Case #"+str(tc+1)+": "+p
