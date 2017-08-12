e59efe8661a135e69571d8f73c6fa75d60d46994
Saran Balaji <csaranbalaji@gmail.com>
2017-08-12 11:26:05 +0530

    Update coloring.py

--- coloring.py
+++ coloring.py
@@ -3,10 +3,10 @@ s=raw_input().strip()
 c=1
 a=s[0]*n
 for i in range(1,len(s)):
-	if a==s:
-		break
-	else:
-		if not(len(a)>i and a[i]==s[i]):
-			a=a[:i]+s[i]*n
-			c+=1
+    if a==s:
+	break
+    else:
+	if not(len(a)>i and a[i]==s[i]):
+	    a=a[:i]+s[i]*n
+	    c+=1
 print c
